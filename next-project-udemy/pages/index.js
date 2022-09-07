8f4a9ea9759310602b839e62f43396b8fa2dfb9d
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-07 11:03:47 +0600

    test code

---
+++ next-project-udemy/pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      
+     <h1>Hello the next world..!</h1>
+     
+    </div>
+  )
+}
