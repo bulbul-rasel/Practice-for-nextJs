a4d3b8e13e930b66f22dba27cfc297d0e5f4b65f
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-19 12:23:16 +0600

    roject start for file base routing

---
+++ next-project-udemy/pages/events/[eventId].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>This is Event detail Page</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
---
+++ next-project-udemy/pages/events/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllEventPage = () => {
+  return (
+    <div>
+      <h1>This is All event Page</h1>
+    </div>
+  );
+};
+
+export default AllEventPage;
