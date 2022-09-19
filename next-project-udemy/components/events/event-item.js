1a446aa861e5adfb4c9bf838fbed10fbd797b308
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-19 14:10:39 +0600

    add styling components

---
+++ next-project-udemy/components/events/event-item.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+import classes from "./event-item.module.css";
+
+const EventItem = ({ title, image, date, location, id }) => {
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedAddress = location.replace(", ", "\n");
+
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title}></img>
+      <div className={classes.content}>
+        <div className={classes.summary}>
+          <h2>{title}</h2>
+        </div>
+        <div className={classes.date}>
+          <time>{humanReadableDate}</time>
+        </div>
+        <div className={classes.address}>
+          <address>{formattedAddress}</address>
+        </div>
+        <div className={classes.actions}>
+          <Link href={exploreLink}> Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default EventItem;
