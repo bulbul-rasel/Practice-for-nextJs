41eaea2d2e9e974d80351f00dacaed97a9a52587
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-19 16:54:23 +0600

    extracting data on catch all route

--- next-project-udemy/pages/events/[...slug].js
+++ next-project-udemy/pages/events/[...slug].js
@@ -1,6 +1,42 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../dummy-data";
 
 const FilteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numYear < 2021 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter, Please adjust your Values..!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.lngth === 0) {
+    return <p> No Events found for the chosen filter</p>;
+  }
+
   return (
     <div>
       <h1>Filtered Event</h1>
--- next-project-udemy/pages/index.js
+++ next-project-udemy/pages/index.js
@@ -4,12 +4,20 @@ import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
 import EventsSearch from "../components/events/events-search";
 import { Fragment } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   const featuredEvents = getFeaturedEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}/abc`;
+
+    router.push();
+  }
   return (
     <Fragment className={styles.container}>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={featuredEvents} />
     </Fragment>
   );
