8cd56be8bb36bb223f08c82a2e765cf2f4ecc02f
bulbul-rasel <rasel.bulbul7@gmail.com>
2022-09-20 12:39:45 +0600

    get serversideprops

---
+++ next-prerendaring-datafatching/pages/user-profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const UserProfilePage = (props) => {
+  return (
+    <div>
+      <h1>{props.username}</h1>
+    </div>
+  );
+};
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: "MAX",
+    },
+  };
+}
